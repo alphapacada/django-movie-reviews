e064a8cbc5c306604441939c2c173adfbf93cf1e
alpha <faipacada@gmail.com>
2022-02-26 17:14:33 +0800

    request from NYT API

--- movie_reviews/reviews/urls.py
+++ movie_reviews/reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Reviews.as_view(), name='index'),
 ]
+
--- movie_reviews/reviews/views.py
+++ movie_reviews/reviews/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.conf import settings
+import requests
+import json
+API_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json'
 # Create your views here.
 def index(request):
 	return render(request, "reviews/index.html")
+
+
+class Reviews(View):
+	
+	template_name = "reviews/index.html"
+	def get(self, request):
+		res = requests.get(API_URL, params={'api-key': settings.NYT_API_KEY, 'critics-pick':'Y'})
+		print(res.request.url)
+		print(res.request.body)
+		print(res.request.headers)
+		print(res)
+		content = {}
+		if res.ok:
+			# limit to 10 reviews
+			content['data'] = res.json()['results']
+			print(type(content['data'][0]))
+		return render(request, self.template_name, content)
+		
