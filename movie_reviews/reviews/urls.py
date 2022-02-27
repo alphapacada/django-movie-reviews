a821c902bc434d8324ff9642d294f47a5e234294
alpha <faipacada@gmail.com>
2022-02-27 08:08:25 +0800

    load 10 more reviews

--- movie_reviews/reviews/urls.py
+++ movie_reviews/reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Reviews.as_view(), name='index'),
+    path('load_more/<int:offset>', views.LoadJSONReview.as_view(), name='load_more')
 ]
 
--- movie_reviews/reviews/views.py
+++ movie_reviews/reviews/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
 from django.views import View
 from django.conf import settings
+from django.http import JsonResponse
 import requests
 import json
 API_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json'
-# Create your views here.
-def index(request):
-	return render(request, "reviews/index.html")
 
+
+def load_reviews(query=None, offset=None, critics_pick='Y'):
+	res = requests.get(API_URL, params={'api-key': settings.NYT_API_KEY, 'critics-pick':critics_pick, 'offset':offset, 'query':query})
+	print(res.request.url)
+	print(res.request.body)
+	print(res.request.headers)
+	print(res)
+	return res
 
 class Reviews(View):
 	
 	template_name = "reviews/index.html"
 	def get(self, request):
-		res = requests.get(API_URL, params={'api-key': settings.NYT_API_KEY, 'critics-pick':'Y'})
-		print(res.request.url)
-		print(res.request.body)
-		print(res.request.headers)
-		print(res)
 		content = {}
+		res = load_reviews()
 		if res.ok:
 			# limit to 10 reviews
-			content['data'] = res.json()['results'][:10]
+			content['data'] = res.json()['results']
 			print(type(content['data'][0]))
 		return render(request, self.template_name, content)
-		
+
+class LoadJSONReview(View):
+	def get(self, request, *args, **kwargs):
+		offset = kwargs.get('offset')
+		res = load_reviews(offset=offset)
+		response = {
+			'data':  res.json()['results']
+		}
+		if response['data']:
+			return JsonResponse(response, status=200)
+		return JsonResponse({}, status = 400)
+
