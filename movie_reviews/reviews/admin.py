c0304e450abcbbfe6f691fc2363b49c49096b3c3
alpha <faipacada@gmail.com>
2022-03-01 06:56:37 +0800

    add associate table Bookmark

--- movie_reviews/reviews/admin.py
+++ movie_reviews/reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
+
+from .models import Collections, Movie, Bookmark
+admin.site.register(Collections)
+admin.site.register(Movie)
+admin.site.register(Bookmark)
+
 # Register your models here.
--- movie_reviews/reviews/models.py
+++ movie_reviews/reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Collections(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="collections")
+    def __str__(self):
+        return self.name
+    class Meta:
+        unique_together = ('name', 'user',)
+
+class Movie(models.Model):
+    display_title=models.CharField(max_length=255)
+    url = models.URLField()
+    img_src = models.URLField()
+    folders = models.ManyToManyField(Collections,through='Bookmark', blank=True)
+    def __str__(self):
+        return self.display_title
+    
+    def check_bookmarked_by(self, user):
+        mov = Movie.objects.filter(folders__user=user)
+        if mov.exists():
+            return True
+        return False
+
+
+class Bookmark(models.Model):
+    folder = models.ForeignKey(Collections, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='bookmarks')
+    date_added = models.DateTimeField(auto_now_add=True)
