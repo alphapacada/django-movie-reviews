95904caf3c6b5c8e00c0f35723caf84b3c149c25
alpha <faipacada@gmail.com>
2022-03-01 07:43:48 +0800

    feature to add bookmark from web page

--- movie_reviews/reviews/urls.py
+++ movie_reviews/reviews/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Reviews.as_view(), name='index'),
-    path('load_more/<int:offset>', views.LoadJSONReview.as_view(), name='load_more')
+    path('load_more/<int:offset>', views.LoadMoreReviews.as_view(), name='load_more'),
+    path('bookmark/', views.add_bookmark, name='add_bookmark'),
 ]
 
--- movie_reviews/reviews/views.py
+++ movie_reviews/reviews/views.py
@@ -17,8 +17,7 @@ from django.views.generic.edit import CreateView, FormView
 from django.core import serializers
 
 
-API_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json'
-
+API_URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json"
 
 
 def load_reviews(user=None, query=None, offset=None, critics_pick=None):
@@ -34,30 +33,36 @@ def load_reviews(user=None, query=None, offset=None, critics_pick=None):
 		list: list of dicts containing movie review details
 	"""
 
-	res = requests.get(API_URL, params={'api-key': settings.NYT_API_KEY, 'critics-pick':critics_pick, 'offset':offset, 'query':query})
-	# print(res.request.url)
-	# print(res.request.body)
-	# print(res.request.headers)
-	# print(res)
+    res = requests.get(
+        API_URL,
+        params={
+            "api-key": settings.NYT_API_KEY,
+            "critics-pick": critics_pick,
+            "offset": offset,
+            "query": query,
+        },
+    )
+
 	res.raise_for_status()
 	if res.ok:
 		
 		data = []
 		
-		for movie in res.json()['results']:
-			obj = {'url':movie['link']['url'],
-				'img_src': movie['multimedia']['src'],
-				'display_title': movie['display_title'],
-				'bookmarked': False}
+        for movie in res.json()["results"]:
+            obj = {
+                "url": movie["link"]["url"],
+                "img_src": movie["multimedia"]["src"],
+                "display_title": movie["display_title"],
+                "bookmarked": False,
+            }
 			if user:
 				# Check if user has bookmarked movie
-				mov = Movie.objects.filter(display_title=movie['display_title'])
+                mov = Movie.objects.filter(display_title=movie["display_title"])
 
 				if mov.exists():
 					if mov[0].check_bookmarked_by(user):
-						obj['bookmarked'] = True
+                        obj["bookmarked"] = True
 			data.append(obj)
-		# check if movie is bookmarked by user
 		return data
 	
 class Reviews(View):
@@ -99,3 +104,17 @@ class LoadMoreReviews(View):
         content["data"] = load_reviews(user=user, offset=offset)
         return render(request, "reviews/movie_list.html", content)
 
+
+@login_required
+def add_bookmark(request):
+    mov, created = Movie.objects.get_or_create(
+        display_title=request.POST.get("display_title"),
+        url=request.POST.get("url"),
+        img_src=request.POST.get("img_src"),
+    )
+
+    q = Bookmark.objects.filter(folder__user=request.user, movie=mov.id)
+    if q.exists():
+		# remove bookmark
+        q.delete()
+    return JsonResponse({}, status=200)
