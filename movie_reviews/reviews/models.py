ef3e79906c1463b8772a12798904723e21e10d9d
alpha <faipacada@gmail.com>
2022-03-01 23:46:43 +0800

    modal popup for saving bookmark to folder

--- movie_reviews/reviews/models.py
+++ movie_reviews/reviews/models.py
@@ -1,24 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Collections(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="collections")
+
     def __str__(self):
         return self.name
     class Meta:
-        unique_together = ('name', 'user',)
+        unique_together = (
+            "name",
+            "user",
+        )
+
 
 class Movie(models.Model):
-    display_title=models.CharField(max_length=255)
+    display_title = models.CharField(max_length=255)
     url = models.URLField()
     img_src = models.URLField()
-    folders = models.ManyToManyField(Collections,through='Bookmark', blank=True)
+    folders = models.ManyToManyField(Collections, through="Bookmark", blank=True)
+
     def __str__(self):
         return self.display_title
-    
+
     def check_bookmarked_by(self, user):
-        mov = Movie.objects.filter(folders__user=user)
+        mov = self.folders.filter(user=user)
         if mov.exists():
             return True
         return False
@@ -26,5 +33,5 @@ class Movie(models.Model):
 
 class Bookmark(models.Model):
     folder = models.ForeignKey(Collections, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='bookmarks')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="bookmarks")
     date_added = models.DateTimeField(auto_now_add=True)
